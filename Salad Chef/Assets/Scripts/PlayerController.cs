8bb8802d0d9ba502e81cb31e70ee5e15e6040df9
Kaso96 <kartik4jan@gmail.com>
2019-05-12 18:16:20 +0530

    Player Movement
    Player Controller Script added. This is attached to both the players. Different Controls for 2 players added in Input Manager and implemented in the script.
    The Control Scheme is decided based on the player number public variable
    The script allows the player to rotate the players on y-axis and move forward

---
+++ Salad Chef/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int m_PlayerNumber;
+    public float m_Speed = 10f;
+    public float m_TurnSpeed = 180f;
+
+    private string m_MovementAxisName;
+    private string m_TurnAxisName;
+    private Rigidbody m_Rigidbody;
+    private float m_MovementInputValue = 0f;
+    private float m_TurnInputValue = 0f;
+
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+    void Start()
+    {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+	}
+
+    private void FixedUpdate()
+    {
+        //Initializing move and rotate function here
+
+        Move();
+        Turn();
+
+    }
+
+    void Move()
+    {
+        //Take the player input to move the player
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+        
+    }
+
+    void Turn()
+    {
+        //Take the player input to rotate the player
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+
+    }
+
+}
